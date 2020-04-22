67459a964cb34b77cc685b128be4f5998d70f2ee
Marcio <marcio-duarte@hotmail.com>
2020-04-22 12:21:31 +0200

    some fake data was added

---
+++ data.py
@@ -0,0 +1,26 @@
+users_list = [
+    {
+        "name": "Marcio",
+        "gender": "male",
+        "postcode": 2400,
+        "country": "Portugal",
+    },
+    {
+        "name": "Dorien",
+        "gender": "female",
+        "postcode": 64178,
+        "country": "Netherlands",
+    },
+    {
+        "name": "Abigail",
+        "gender": "female",
+        "postcode": 54678,
+        "country": "Canada"
+    },
+    {
+        "name":"Efe",
+        "gender": "male",
+        "postcode": 5842,
+        "country": "Turkey"
+    }
+]
--- server.py
+++ server.py
@@ -1,28 +1,12 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+import data as users; 
 
 app = Flask(__name__)
 api = Api(app)
+users = users.users_list
 
-users = [
-    {
-        "name": "Marcio",
-        "age": 39,
-        "nationality": "Portuguese"
-    },
-    {
-        "name": "Bisrate",
-        "age": "130",
-        "Ocupation": "DCI"
-    },
-    {
-        "name": "Younes",
-        "age": 200,
-        "occupation": "Student at DCI"
-    }
-]
-
-
+# -- Routes -- #
 class User(Resource):
     def get(self, name):
         for user in users:
@@ -30,13 +14,12 @@ class User(Resource):
                 return user, 200
         return "Name not found", 404
 
-
 class AllUsers(Resource):
     def get(self):
         return users, 200
 
-
 api.add_resource(User, "/name/<string:name>")
 api.add_resource(AllUsers, "/all")
 
+# -- Run and debug the Flask server -- #
 app.run(debug=True, host='0.0.0.0', port=8888)
